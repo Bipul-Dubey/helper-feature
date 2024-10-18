2c84c6495b6d156f2913496742ffc8cd310ff677
Bipul Dubey <bipul0873@gmail.com>
2024-10-18 23:15:36 +0530

    fix: static render attempt 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV == 'production';
+
 const nextConfig = {
+  basePath: isProd ? '/helper-feature' : '',
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
+  basePath: '',
 };
 
 export default nextConfig;
